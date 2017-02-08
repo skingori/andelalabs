c42cd14f6bd589dd8bbfcf6e45ba91761e938b4c
skingori <king@kings-MacBook-Pro.local>
2017-02-08 18:55:11 +0300

    Added input for API key and City Name

--- openweatherapi/weather.py
+++ openweatherapi/weather.py
@@ -1,1 +1,25 @@
+import requests
+import json
+import time
 
+
+def convert_kel_cel(kel_value):
+    return int(kel_value - 273.15)
+print("Enter City Below")
+city = input("")
+print("Enter your API KEY below")
+key = input('')
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key
+
+response = requests.get(url)
+
+content = response.content
+
+data = json.loads(content)
+
+temp = data['main']['temp']
+sky=data['weather'][0]['main']
+pr=data['main']['pressure']
+
+'''Get the main data'''
+print ("The weather in " + city + " is: " + str(convert_kel_cel(temp)) + '°' + " ,Sky:" + sky + " ,Pressure:" +str(convert_kel_cel(pr)))
